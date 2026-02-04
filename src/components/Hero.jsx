c88049d1c5205fb4787966bce2de61550d350f13
gill-amitoj <amitoj3@ualberta.ca>
2026-02-03 20:39:50 -0700

    fix: remove offset box behind profile photo for cleaner look

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -90,11 +90,10 @@ export default function Hero() {
             className="hidden lg:flex justify-center items-center"
           >
             <div className="relative">
-              <div className="absolute inset-0 border border-light translate-x-4 translate-y-4" />
               <img
                 src="/profile.png"
                 alt="Amitoj Singh"
-                className="w-80 h-80 object-cover border border-light grayscale hover:grayscale-0 transition-all duration-500"
+                className="w-80 h-80 object-cover border-2 border-light grayscale hover:grayscale-0 transition-all duration-500"
               />
             </div>
           </motion.div>
